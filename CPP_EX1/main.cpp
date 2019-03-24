164d0c8961c5226e4c6684d3ea17f144344b4347
othman <othman_watad@hotmail.com>
2019-03-24 11:17:50 +0200

    Initial Commit

---
+++ CPP_EX1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_EX1
+//
+//  Created by othman wattad on 24.03.19.
+//  Copyright © 2019 othman wattad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
