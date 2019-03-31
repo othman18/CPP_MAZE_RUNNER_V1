17ecadfd41ec50296c08c46ae26bd9e2792935c5
othman <othman_watad@hotmail.com>
2019-03-31 17:49:38 +0300

    small change

--- src/extractMaze.cpp
+++ src/extractMaze.cpp
@@ -159,3 +159,4 @@ bool is_number(const std::string& s){
     return !s.empty() && std::find_if(s.begin(),
                                       s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
 }
+
